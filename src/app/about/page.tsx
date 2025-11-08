a781367f5d303abf67670be9183894667fd09647
MadhukaRuchiranga <madhukaruchiranga2019@gmail.com>
2025-11-08 13:49:56 +0530

    create the about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        aspernatur et ratione ducimus quasi sit ad voluptate officia maiores
+        expedita neque sed, architecto culpa debitis quos doloremque accusamus
+        nihil ut illo dolorum deleniti dicta recusandae officiis. Modi eius
+        beatae a numquam. Obcaecati laboriosam eaque veniam? Expedita impedit
+        dolores tempore tempora!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+        asperiores eaque! Accusantium asperiores qui repellat cum culpa eius?
+        Corrupti vero quam quis, reiciendis dolore numquam perferendis!
+        Exercitationem animi maxime tempora fuga at officia a excepturi enim
+        aliquam quidem labore, quisquam accusantium fugit totam, temporibus
+        facere! Aspernatur quasi eveniet ratione non velit quibusdam saepe, quia
+        esse reprehenderit doloremque sequi, omnis molestias! Pariatur
+        distinctio quos sunt veniam laborum neque, nostrum aut. Nobis cupiditate
+        ipsum ipsam praesentium dicta. Minima nulla quibusdam nihil ipsam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        distinctio quam totam eligendi rem. Cum quaerat dolor voluptas incidunt
+        odit blanditiis earum repellendus? Culpa quam, dicta officiis placeat
+        similique possimus? Odit, quos autem assumenda ab sed quidem nostrum
+        veniam laborum rerum aliquid. Reiciendis quo temporibus quis aliquid
+        omnis laborum magnam, suscipit ullam tempora totam assumenda, eos a quam
+        ex nisi eius sequi earum. Ea, nam molestias soluta excepturi dolorum
+        inventore quidem facere explicabo sint quibusdam adipisci distinctio
+        voluptate labore sit deleniti quis a, ipsam nemo dolores quo, fugit
+        tenetur voluptatibus illum odit? Deserunt praesentium ducimus magni cum
+        laudantium nihil deleniti quos pariatur, omnis alias nemo veniam impedit
+        cupiditate facilis ipsam?
+      </p>
+    </div>
+  );
+}
